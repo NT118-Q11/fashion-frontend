10dadfc501bca8231be2fbb8e6d1b58c42f0ab00
tuan-te <taquoctuan01102005@gmail.com>
2025-11-21 09:39:11 +0700

    reslove conflict

---
+++ app/src/main/java/com/example/fashionapp/uix/SearchFragment.kt
@@ -0,0 +1,51 @@
+package com.example.fashionapp.uix
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fashionapp.R
+import androidx.navigation.fragment.findNavController
+import com.example.fashionapp.databinding.ActivitySearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: ActivitySearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ActivitySearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.navHome.setOnClickListener {
+            findNavController().navigate(R.id.action_activitySearchFragment_to_homeFragment)
+        }
+
+        binding.navProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_activitySearchFragment_to_myAccountFragment)
+        }
+
+        binding.navCart.setOnClickListener {
+            findNavController().navigate(R.id.action_activitySearchFragment_to_cartFragment)
+        }
+
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
